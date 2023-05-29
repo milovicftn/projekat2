47f5335b10f83dd0cd2a909086520fe63d3d3b7a
milovicftn <milovic.m.ftn@gmail.com>
2023-05-29 04:08:56 +0200

    Postavljanje projekta empty

---
+++ projekat2.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: MarkoSSD
+ *
+ * Created on 29. maj 2023., 04.05
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
